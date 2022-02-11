6976eb71efbf43f06213ae302385c52cb094df58
Aram Minasyan <73815175+159753852123@users.noreply.github.com>
2022-02-11 10:59:36 +0400

    Delete dns-zone directory

--- dns-zone/main.tf
+++
@@ -1,6 +0,0 @@
-resource "google_dns_managed_zone" "this" {
-  project     = var.project_id
-  name        = replace(var.name, ".", "-")
-  dns_name    = var.name
-  description = var.description
-}
